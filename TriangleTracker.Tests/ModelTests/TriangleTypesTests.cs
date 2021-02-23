8d158f5c0fb9b924c82369824c7943c37aa25ba6
Nick Lindau <nicholaithegreat@gmail.com>
2021-02-23 11:09:56 -0800

    pass third test Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleTypesTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTypesTests.cs
@@ -14,11 +14,18 @@ namespace TriangleTracker.TestTools
     }
 
     [TestMethod]
-
     public void Triangle_AreAllSidesEqual_True()
     {
       Triangle testTriangle = new Triangle(4, 4, 4);
       Assert.AreEqual("Is an equilateral triangle", testTriangle.IsATriangle());
+    }
+
+    [TestMethod]
+
+    public void Triangle_TwoSidesAreEqual_True()
+    {
+      Triangle testTriangle = new Triangle(4, 4, 7);
+      Assert.AreEqual("Is an isosceles triangle", testTriangle.IsATriangle());
     }
   }
 }
